19a94a4af3a0ddb7396a0f651647ec9d585e1fb4
Shawn Thompson <plansmash@me.com>
2017-03-23 13:03:15 -0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,123 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var header = require('gulp-header');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+var pkg = require('./package.json');
+var ghPages = require('gulp-gh-pages');
+
+// Set the banner content
+var banner = ['/*!\n',
+    ' * <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+    ' * Copyright' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+    ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
+    ' */\n',
+    ''
+].join('');
+
+// Compile SASS files from /scss into /css
+gulp.task('sass', function() {
+    return gulp.src('scss/artisan.scss')
+        .pipe(sass())
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Minify compiled CSS
+gulp.task('minify-css', ['sass'], function() {
+    return gulp.src('dist/css/artisan.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Copy vendor libraries from /node_modules into /vendor
+gulp.task('copy', function() {
+    gulp.src(['*.html'])
+        .pipe(gulp.dest('dist'))
+
+    gulp.src(['js/**/*.js'])
+        .pipe(gulp.dest('dist/js'))
+
+    gulp.src(['img/**/*.*'])
+        .pipe(gulp.dest('dist/img'))
+
+    gulp.src(['mail/*.*'])
+        .pipe(gulp.dest('dist/mail'))
+
+    gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
+        .pipe(gulp.dest('dist/vendor/bootstrap'))
+
+    gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+        .pipe(gulp.dest('dist/vendor/jquery'))
+
+    gulp.src([
+            'node_modules/font-awesome/**',
+            '!node_modules/font-awesome/**/*.map',
+            '!node_modules/font-awesome/.npmignore',
+            '!node_modules/font-awesome/*.txt',
+            '!node_modules/font-awesome/*.md',
+            '!node_modules/font-awesome/*.json'
+        ])
+        .pipe(gulp.dest('dist/vendor/font-awesome'))
+})
+
+// Minify JS
+gulp.task('minify-js', function() {
+    return gulp.src('dist/js/artisan.js')
+        .pipe(uglify())
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Run everything
+gulp.task('default', ['sass', 'minify-css', 'minify-js', 'copy']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'dist'
+        },
+    })
+})
+
+// Dev task with browserSync
+gulp.task('dev', ['browserSync', 'sass', 'minify-css', 'minify-js'], function() {
+    gulp.watch('scss/*.scss', ['sass']);
+    gulp.watch('css/*.css', ['minify-css']);
+    gulp.watch('js/*.js', ['minify-js']);
+    // Reloads the browser whenever HTML or JS files change
+    gulp.watch('*.html', browserSync.reload);
+    gulp.watch('js/**/*.js', browserSync.reload);
+});
+
+// Compiles SCSS files from /scss into /css
+// NOTE: This theme uses LESS by default. To swtich to SCSS you will need to update this gulpfile by changing the 'less' tasks to run 'sass'!
+gulp.task('sass', function() {
+    return gulp.src('scss/artisan.scss')
+        .pipe(sass())
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Copy the dist folder to a gh-pages branch
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages());
+});
---
+++ notes.md
@@ -0,0 +1,15 @@
+# Notes
+
+## Image sizes
+
+Main header image: 1900px x 1250px
+
+About: 200px x 200px
+
+Partners: 200px x 50px
+
+Portfolio thumbs: 400px x 286px
+
+Staff: 225px x 225px
+
+Other sections: 1469px x 720px
