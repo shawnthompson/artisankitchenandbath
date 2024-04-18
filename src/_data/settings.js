152ffde445c6c3a3e7b51e37273cf535d8177644
Shawn Thompson <plansmash@me.com>
2024-04-18 16:50:37 -0400

    add site info to settings data

--- src/_data/settings.js
+++ src/_data/settings.js
@@ -2,4 +2,8 @@ module.exports = {
 	buildTime: new Date(),
 	env: process.env.ELEVENTY_ENV,
 	baseUrl: "/",
-};
+	site: {
+		title: "Artisan Kiten and Bath",
+		description: "Artisan Wood Designs offers personalized service from design, production and installation. We hold ourselves to the highest standards of craftsmanship, creativity and professionalism. Whether you are planning a new home or remodeling your present home, the exceptional quality and elegance of our work will enhance both its beauty and its value.",
+	}
+}
