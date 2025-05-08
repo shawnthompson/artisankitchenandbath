2aa8f803a7241fce6e206853407c3fc9ed06594a
Shawn Thompson <plansmash@me.com>
2025-05-08 10:00:46 -0400

    change business name

--- src/_data/settings.js
+++ src/_data/settings.js
@@ -4,6 +4,6 @@ module.exports = {
 	baseUrl: "/",
 	site: {
 		title: "Artisan Kiten and Bath",
-		description: "Artisan Wood Designs offers personalized service from design, production and installation. We hold ourselves to the highest standards of craftsmanship, creativity and professionalism. Whether you are planning a new home or remodeling your present home, the exceptional quality and elegance of our work will enhance both its beauty and its value.",
+		description: "Artisan Kitchen and Bath offers personalized service from design, production and installation. We hold ourselves to the highest standards of craftsmanship, creativity and professionalism. Whether you are planning a new home or remodeling your present home, the exceptional quality and elegance of our work will enhance both its beauty and its value.",
 	}
 }
